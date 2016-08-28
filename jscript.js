3a920aae78e7603789507dae77510eaf2fec06bb
Michael Lawrence <m6lawrence1@gmail.com>
2016-08-28 17:06:33 -0400

    condensed jquery functions, added tweet button for random quote

--- jscript.js
+++ jscript.js
@@ -1,46 +1,39 @@
+var quote;
+var author;
 
+function getQuote (){
+    
+    $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(Math.floor((Math.random() * 300) + 1)); // The data is an array of posts. Grab a random post.
+        $('#quote-title').text(post.title);
+        $('#quote-content').html(post.content);
+         var quote = $('#quote-content').text().trim(); //trim removes any white spaces on both sides of string
+          var author = $('#quote-title').text();
+          
+        
+        // If the Source is available, use it. Otherwise hide it.
+        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+          $('#quote-source').html('Source:' + post.custom_meta.Source);
+        } else {
+          $('#quote-source').text('');
+        }
+      
+        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ~' + author));  
+          
+      },
+      cache: false
+    });
+    
+    
+}
 $(function () {
-    
-    $( window ).load(function(e) {
-    e.preventDefault();
-    $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-      success: function(data) {
-        var post = data.shift(); // The data is an array of posts. Grab the first one.
-        $('#quote-title').text(post.title);
-        $('#quote-content').html(post.content);
-
-        // If the Source is available, use it. Otherwise hide it.
-        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-          $('#quote-source').html('Source:' + post.custom_meta.Source);
-        } else {
-          $('#quote-source').text('');
-        }
-      },
-      cache: false
-    });
- 
-    
-    
-$('#get-another-quote-button').on('click', function(e) {
-    e.preventDefault();
-    $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-      success: function(data) {
-        var post = data.shift(); // The data is an array of posts. Grab the first one.
-        $('#quote-title').text(post.title);
-        $('#quote-content').html(post.content);
-
-        // If the Source is available, use it. Otherwise hide it.
-        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-          $('#quote-source').html('Source:' + post.custom_meta.Source);
-        } else {
-          $('#quote-source').text('');
-        }
-      },
-      cache: false
-    });
-  });
-});
-
- });
+    getQuote();
+    $('#get-another-quote-button').on('click', getQuote);
+    $('#tweet-quote').on('click', function() {
+    if(!inIframe()) {
+      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ' + author));
+   }
+ }); 
+})
