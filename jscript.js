055ebbb88890d1a873cea3ba75e3beb3474d1286
Michael Lawrence <m6lawrence1@gmail.com>
2016-08-28 17:13:00 -0400

    removed source option for quote

--- jscript.js
+++ jscript.js
@@ -1,29 +1,21 @@
 var quote;
 var author;
 
-function getQuote (){
+function getQuote() {
     
-    $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-      success: function(data) {
-        var post = data.shift(Math.floor((Math.random() * 300) + 1)); // The data is an array of posts. Grab a random post.
-        $('#quote-title').text(post.title);
-        $('#quote-content').html(post.content);
-         var quote = $('#quote-content').text().trim(); //trim removes any white spaces on both sides of string
-          var author = $('#quote-title').text();
+    $.ajax({
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        success: function (data) {
+            var post = data.shift(Math.floor((Math.random() * 300) + 1)); // The data is an array of posts. Grab a random post.
+            $('#quote-title').text(post.title);
+            $('#quote-content').html(post.content);
+            var quote = $('#quote-content').text().trim(); //trim removes any white spaces on both sides of string
+            var author = $('#quote-title').text();
           
-        
-        // If the Source is available, use it. Otherwise hide it.
-        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-          $('#quote-source').html('Source:' + post.custom_meta.Source);
-        } else {
-          $('#quote-source').text('');
-        }
       
-        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ~' + author));  
-          
-      },
-      cache: false
+            $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ~ ' + author));
+        },
+        cache: false
     });
     
     
@@ -31,9 +23,7 @@ function getQuote (){
 $(function () {
     getQuote();
     $('#get-another-quote-button').on('click', getQuote);
-    $('#tweet-quote').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ' + author));
-   }
- }); 
-})
+    $('#tweet-quote').on('click', function () {
+            openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ' + author));       
+    });
+});
